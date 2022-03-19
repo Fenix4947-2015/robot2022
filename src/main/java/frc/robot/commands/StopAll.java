ae208133d8e6e2e9fd68ee47799ec3b801e05a27
fross <fross.job@gmail.com>
2022-03-19 14:43:41 -0400

    Ajoute l'intake (système + commandes)

--- src/main/java/frc/robot/commands/StopAll.java
+++ src/main/java/frc/robot/commands/StopAll.java
@@ -1,20 +1,28 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
 
 public class StopAll extends InstantCommand {
 
     private final DriveTrain m_driveTrain;
+    private final Intake m_intake;
 
-    public StopAll(DriveTrain driveTrain) {
+    public StopAll(DriveTrain driveTrain, Intake intake) {
         m_driveTrain = driveTrain;
+        m_intake = intake;
 
         addRequirements(driveTrain);
+        addRequirements(intake);
     }
 
     @Override
     public void execute() {
         m_driveTrain.stop();
+        m_intake.stop();
+
+        CommandScheduler.getInstance().cancelAll();
     }
 }
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.InstakeConstants;
+
+public class Intake extends SubsystemBase {
+
+    private final CANSparkMax m_motor = new CANSparkMax(InstakeConstants.kMotorDeviceId, MotorType.kBrushless);
+
+    private final RelativeEncoder m_encoder = m_motor.getEncoder();
+
+    public Intake() {
+        m_motor.restoreFactoryDefaults();
+        m_motor.setIdleMode(IdleMode.kBrake);
+    }
+
+    public void roll(double speed) {
+        m_motor.set(speed);
+    }
+
+    public void stop() {
+        m_motor.set(0.0);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Speed (RPM)", m_encoder.getVelocity());
+        SmartDashboard.putNumber("Bus Voltage", m_motor.getBusVoltage());
+        SmartDashboard.putNumber("Current", m_motor.getOutputCurrent());
+        SmartDashboard.putNumber("Applied Output", m_motor.getAppliedOutput());
+        SmartDashboard.putNumber("Temperature (C)", m_motor.getMotorTemperature());
+    }
+}
