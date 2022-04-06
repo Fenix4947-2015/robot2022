220d7a9ea5de0a250e8a3f7fde732995a0c35943
etdube <etdube@gmail.com>
2022-04-06 13:07:20 -0400

    Add SysId config

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -51,11 +51,11 @@ public class DriveTrain extends SubsystemBase {
 
         m_drive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
 
-        m_leftLeaderEncoder.setPositionConversionFactor(DriveTrainConstants.kEncoderPositionConversionFactor);
-        m_rightLeaderEncoder.setPositionConversionFactor(DriveTrainConstants.kEncoderPositionConversionFactor);
+        m_leftLeaderEncoder.setPositionConversionFactor(DriveTrainConstants.kEncoderPositionConversionFactorHigh);
+        m_rightLeaderEncoder.setPositionConversionFactor(DriveTrainConstants.kEncoderPositionConversionFactorHigh);
 
-        m_leftLeaderEncoder.setVelocityConversionFactor(DriveTrainConstants.kEncoderVelocityConversionFactor);
-        m_rightLeaderEncoder.setVelocityConversionFactor(DriveTrainConstants.kEncoderVelocityConversionFactor);
+        m_leftLeaderEncoder.setVelocityConversionFactor(DriveTrainConstants.kEncoderVelocityConversionFactorHigh);
+        m_rightLeaderEncoder.setVelocityConversionFactor(DriveTrainConstants.kEncoderVelocityConversionFactorHigh);
 
         m_spareTalon.configFactoryDefault();
         m_gyro.configFactoryDefault();
